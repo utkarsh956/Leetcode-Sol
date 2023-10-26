4823c11a48050e5a83aaabca74151680edebcfbc
Utkarsh Saxena <148000798+utkarsh956@users.noreply.github.com>
2023-10-26 20:09:11 +0530

    Time: 13 ms (30.60%), Space: 6.5 MB (6.34%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,13 @@
+class Solution
+{
+    public:
+        int helper(int n)
+        {
+            if (n <= 1) return n;
+            return helper(n - 1) + helper(n - 2);
+        }
+    int fib(int n)
+    {
+        return helper(n);
+    }
+};
